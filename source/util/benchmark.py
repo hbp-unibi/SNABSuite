a03bcf0277e864fa15db6231a5490e0ef2c3d95b
Christoph Ostrau <costrau@techfak.uni-bielefeld.de>
2019-03-19 09:33:54 +0100

    Benchmark web service: Robustness against upper and lower case in simulator names

--- source/util/benchmark.py
+++ source/util/benchmark.py
@@ -6,8 +6,10 @@ arguments = sys.argv[1:]
 simulators = ["nest", "spinnaker", "nmpm1", "spikey"]  # SNABSuite simulators
 web_simulators = ["NEST", "SpiNNaker", "BrainScaleS",
                   "Spikey"]  # Web frontend simulators
+web_simulators_low = ["nest", "spinnaker", "brainscales",
+                  "spikey"]  # Web frontend simulators
 
-index = web_simulators.index(arguments[0])
+index = web_simulators_low.index(arguments[0].lower())
 arguments[0] = simulators[index]
 
 args_list = ["./benchmark"]
