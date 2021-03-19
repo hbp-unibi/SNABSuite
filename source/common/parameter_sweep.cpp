d469184285cb438b302f6e74ed07ecdb9ebc052d
Christoph Ostrau <costrau@techfak.uni-bielefeld.de>
2021-03-19 10:07:48 +0100

    improve structure of sweep configurations

--- source/common/parameter_sweep.cpp
+++ source/common/parameter_sweep.cpp
@@ -225,7 +225,7 @@ ParameterSweep::ParameterSweep(std::string backend, cypress::Json &config,
     : m_backend(backend)
 {
 	std::string snab_name = config["snab_name"];
-	m_sweep_config = extract_backend(config, m_backend);
+	m_sweep_config = extract_backend(config["config"], m_backend);
 	// Get the correct SNAB
 	auto snab_vec = snab_registry(m_backend, bench_index);
 	for (auto i : snab_vec) {
